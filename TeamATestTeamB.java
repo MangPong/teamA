90de47d90132ae2a84130fe9f09635003b8487b5
MangPong <mangpong.eiei@gmail.com>
2024-12-23 20:04:41 +0700

    Test pass

--- TeamATestTeamB.java
+++ TeamATestTeamB.java
@@ -4,7 +4,7 @@ import java.nio.file.Paths;
 import java.io.IOException;
 import org.junit.Test;
 
-public class TeamBTest {
+public class TeamATestTeamB {
 
     @Test
     public void testCalculateAge() {
